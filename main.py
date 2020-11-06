cc2b0a913ca7a5f3dc2f844139dcae78febdbcfc
Leticia Fritz <leticiafritz@id.uff.br>
2020-11-06 16:43:27 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+############################################################
+# PROJETO: SOLUÇÃO DO FLUXO DE CARGA
+# NOME: LETÍCIA FRITZ HENRIQUE
+# E-MAIL: LETICIA.HENRIQUE@UFJF.BR
+# VERSÃO: 0.0
+############################################################
+
+# BIBLIOTECAS
+from ybus import Ybus
+
+
+# FUNÇÃO PRINCIPAL
+def main():
+    # Dados de Entrada
+    print("ALGORITMO PARA SOLUÇÃO DO FLUXO DE CARGA")
+    print("-----")
+
+
+# RUN
+if __name__ == '__main__':
+    main()
---
+++ ybus.py
@@ -0,0 +1,3 @@
+class Ybus:
+    def __init__(self, data):
+        self.data = data
